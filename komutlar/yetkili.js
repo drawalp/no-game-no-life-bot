a7e9936f850fd0a1ea04158085f558481424fb45
drawalp <46400818+drawalp@users.noreply.github.com>
2019-01-05 18:56:12 +0300

    Update yetkili.js

--- komutlar/yetkili.js
+++ komutlar/yetkili.js
@@ -7,7 +7,7 @@ exports.run = function(client, message, args) {
 new Discord.RichEmbed()
 .setColor('RANDOM')
 .setTitle('NoGameNoLife Yetkili Komutları')
-.setDescription('`?çekiliş` **Çekiliş Yaparsın.**\n`?emojiyazı <yazı>` **Emoji İle Yazılar Yazdırırsın.**\n`?istatistik` **Botun İstatistiğine Bakarsın.**\n`?oylama` **Oylama Yaparsın.**\n`?ping` **Botun Pingine Bakarsın.**\n`?temizle <0/100>` **Sohbeti Temizler.**\n`?ban @kullanıcı <sebep>` **Kullanıcıyı Sunucudan Yasaklarsın.**\n`?kick @kullanıcı <sebep>` **Kullanıcıyı Sunucudan Atarsın.**\n`?kilit <süre>` **Belirlediğin Süre Kadar Komutu Yazdığın Kanalı Kilitler.**\n`?rol-ver @kullanıcı @rol` **Kullanıcıya Rol Verir.**\n`?yavaşmod <süre>` **Komutu Kullandığın Kanalda Yavaş Modu Belirlediğin Süre Kadar Açar.**\n`?sustur @kullanıcı <sebep>` **Seçtiğin Kullanıcıyı Susturur.(Kullanmak İçin `Susturulmuş` Diye Rol Oluşturup Yetkisini Ayarlamanız Gerekir.)**'));
+.setDescription('`?çekiliş` **Çekiliş Yaparsın.**\n`?emojiyazı <yazı>` **Emoji İle Yazılar Yazdırırsın.**\n`?istatistikler` **Botun İstatistiğine Bakarsın.**\n`?oylama` **Oylama Yaparsın.**\n`?ping` **Botun Pingine Bakarsın.**\n`?temizle <0/100>` **Sohbeti Temizler.**\n`?ban @kullanıcı <sebep>` **Kullanıcıyı Sunucudan Yasaklarsın.**\n`?kick @kullanıcı <sebep>` **Kullanıcıyı Sunucudan Atarsın.**\n`?kilit <süre>` **Belirlediğin Süre Kadar Komutu Yazdığın Kanalı Kilitler.**\n`?rol-ver @kullanıcı @rol` **Kullanıcıya Rol Verir.**\n`?yavaşmod <süre>` **Komutu Kullandığın Kanalda Yavaş Modu Belirlediğin Süre Kadar Açar.**\n`?sustur @kullanıcı <sebep>` **Seçtiğin Kullanıcıyı Susturur.(Kullanmak İçin `Susturulmuş` Diye Rol Oluşturup Yetkisini Ayarlamanız Gerekir.)**'));
 const embed = new Discord.RichEmbed()
 .setColor('RANDOM')
 .setDescription('')
